c689d3a56a57f61e3ce772f4ea80d201051f4e1f
stephenpalgeorge <stephenpalgeorge@gmail.com>
2021-01-19 16:59:00 +0000

    install packages and register middleware

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,22 @@
-import * as express from 'express';
-import * as path from 'path';
 import * as bodyParser from 'body-parser';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
 import * as mongoose from 'mongoose';
+import * as path from 'path';
 import { Message } from '@in-the-house/api-interfaces';
 
+// define initial variables
 const app = express();
 const PORT = process.env.port || 3333;
 const URI = `mongodb+srv://stephenpg:${process.env.DB_PASSWORD}@cluster0.2hnpx.mongodb.net/ith-database?retryWrites=true&w=majority`;
 
+/**
+ * connect to the database
+ * all subsequent app code is run in the `then` block, so the server will not start
+ * if the db cannot be reached.
+ * 
+ */
 mongoose.connect(URI, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -16,23 +25,29 @@ mongoose.connect(URI, {
 })
 .then(() => {
   console.log('successfully connected to the DB...');
+  // define static assets
   app.use(express.static(path.join(__dirname, '..', 'in-the-house')));
+  // register middleware
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(compression());
+  app.use(cookieParser());
   
   const greeting: Message = { message: 'Welcome to api!' };
-  
+  // API ROUTES
   app.get('/api', (_, res: express.Response) => {
     res.send(greeting);
   });
   
+  // Catch all other routes and serve the React app:
   app.get('/*', (_, res: express.Response) => {
     res.sendFile(path.join(__dirname, '..', 'in-the-house', 'index.html'));
   });
   
+  // start the server
   const server = app.listen(PORT, () => {
-    console.log('Listening at http://localhost:' + PORT + '/api');
+    console.log(`Listening at http://localhost: ${PORT}`);
   });
   server.on('error', console.error);
 })
-.catch(err => console.error(err));
+.catch(console.error);
