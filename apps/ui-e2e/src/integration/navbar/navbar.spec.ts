ab602a35e5a92dcc270768fe97b8fff86c8be232
stephenpalgeorge <stephenpalgeorge@gmail.com>
2021-01-23 23:04:55 +0000

    story book sass setup

--- apps/ui-e2e/src/integration/navbar/navbar.spec.ts
+++ apps/ui-e2e/src/integration/navbar/navbar.spec.ts
@@ -1,5 +1,5 @@
 describe('ui: Navbar component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=navbar--primary'));
+  beforeEach(() => cy.visit('/iframe.html?id=navbar--primary&knob-menu&knob-actions'));
     
     it('should render the component', () => {
       cy.get('h1').should('contain', 'Welcome to ui!');
