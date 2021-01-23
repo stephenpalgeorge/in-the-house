76d60ff32f86a66aeb8f2d6e5588d6b6044f8ab2
stephenpalgeorge <stephenpalgeorge@gmail.com>
2021-01-23 23:31:54 +0000

    back out story book

--- apps/ui-e2e/src/integration/navbar/navbar.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('ui: Navbar component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=navbar--primary&knob-menu&knob-actions'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to ui!');
-    });
-});
