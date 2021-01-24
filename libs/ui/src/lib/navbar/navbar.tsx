42d0e17d5619fea5a0de3dd83041ffc2c16f0d08
stephenpalgeorge <stephenpalgeorge@gmail.com>
2021-01-24 22:45:56 +0000

    menu toggle on icon click

--- libs/ui/src/lib/navbar/navbar.tsx
+++ libs/ui/src/lib/navbar/navbar.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import './navbar.scss';
+import { Link, useLocation } from 'react-router-dom';
 
 export interface ILink {
   path: string,
@@ -15,20 +13,40 @@ export interface NavbarProps {
 
 export function Navbar(props: NavbarProps) {
   const { actions, menu } = props;
+  const location = useLocation();
+  const navBarRef = React.useRef(null);
+  const menuRef = React.useRef(null);
+  const menuIconRef = React.useRef(null);
+
+  const toggleMenu = () => {
+    navBarRef.current.classList.toggle('open');
+    menuRef.current.classList.toggle('open');
+    menuIconRef.current.classList.toggle('open');
+  }
+
   return (
-    <nav className="navbar">
-      <ul className="menu">
+    <nav ref={ navBarRef } className="navbar">
+      <ul ref={ menuRef } className="menu">
         {
           (menu.length > 0) &&
-          menu.map((item, i) => <li key={i}><Link to={item.path}>{ item.label }</Link></li>)
+          menu.map((item, i) => {
+            return <li className={location.pathname === item.path ? 'active' : ''} key={i}>
+              <Link to={item.path}>{ item.label }</Link>
+            </li>
+          })
         }
       </ul>
-      {
-        (actions && actions.length > 0 && actions.length < 3) &&
-        <ul className="actions">
-          { actions.map((action, i) => <li key={i}><Link to={action.path}>{ action.label }</Link></li>) }
-        </ul>
-      }
+      <ul className="actions">
+        <li ref={ menuIconRef } id="menu-icon" onClick={ toggleMenu }>
+          <div className="icon-bar" id="icon-bar--top"></div>
+          <div className="icon-bar" id="icon-bar--middle"></div>
+          <div className="icon-bar" id="icon-bar--bottom"></div>
+        </li>
+        {
+          (actions && actions.length > 0 && actions.length < 3) &&
+          actions.map((action, i) => <li key={i}><Link to={action.path}>{ action.label }</Link></li>)
+        }
+      </ul>
     </nav>
   );
 }
