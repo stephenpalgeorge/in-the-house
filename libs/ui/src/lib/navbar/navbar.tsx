affae1470e7bd021d505d31fa44c23f776257642
stephenpalgeorge <stephenpalgeorge@gmail.com>
2021-01-24 23:47:19 +0000

    block component working

--- libs/ui/src/lib/navbar/navbar.tsx
+++ libs/ui/src/lib/navbar/navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { Block } from '../block/block';
 
 export interface ILink {
   path: string,
@@ -27,27 +28,29 @@ export function Navbar(props: NavbarProps) {
 
   return (
     <nav ref={ navBarRef } className="navbar">
-      <ul ref={ menuRef } className="menu">
-        {
-          (menu.length > 0) &&
-          menu.map((item, i) => {
-            return <li className={location.pathname === item.path ? 'active' : ''} key={i}>
-              <Link to={item.path}>{ item.label }</Link>
-            </li>
-          })
-        }
-      </ul>
-      <ul className="actions">
-        <li ref={ menuIconRef } id="menu-icon" onClick={ toggleMenu }>
-          <div className="icon-bar" id="icon-bar--top"></div>
-          <div className="icon-bar" id="icon-bar--middle"></div>
-          <div className="icon-bar" id="icon-bar--bottom"></div>
-        </li>
-        {
-          (actions && actions.length > 0 && actions.length < 3) &&
-          actions.map((action, i) => <li className={`action ${'background-color--' + action.color || ''}`} key={i}><Link to={action.path}>{ action.label }</Link></li>)
-        }
-      </ul>
+      <Block contents="narrow">
+        <ul ref={ menuRef } className="menu">
+          {
+            (menu.length > 0) &&
+            menu.map((item, i) => {
+              return <li className={location.pathname === item.path ? 'active' : ''} key={i}>
+                <Link to={item.path}>{ item.label }</Link>
+              </li>
+            })
+          }
+        </ul>
+        <ul className="actions">
+          <li ref={ menuIconRef } id="menu-icon" onClick={ toggleMenu }>
+            <div className="icon-bar" id="icon-bar--top"></div>
+            <div className="icon-bar" id="icon-bar--middle"></div>
+            <div className="icon-bar" id="icon-bar--bottom"></div>
+          </li>
+          {
+            (actions && actions.length > 0 && actions.length < 3) &&
+            actions.map((action, i) => <li className={`action ${'background-color--' + action.color || ''}`} key={i}><Link to={action.path}>{ action.label }</Link></li>)
+          }
+        </ul>
+      </Block>
     </nav>
   );
 }
