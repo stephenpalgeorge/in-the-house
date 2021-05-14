7bd1445158449363aef1864f278d1a77b83745c5
stephenpg <stephenpalgeorge@gmail.com>
2021-05-14 15:15:47 +0100

    Fix syntax error in email template

--- apps/api/src/app/_data/email-templates/verify-email.template.ts
+++ apps/api/src/app/_data/email-templates/verify-email.template.ts
@@ -8,7 +8,7 @@ export function verifyEmail(info: ITemplateFuncSignature): IEmailTemplate {
     sender: EmailSender.admin,
     subject: "Verify your email address",
     contents: `
-      <h1>{user.firstname && user.firstname.length > 0 ? user.firstname : user.username}</h1>
+      <h1>Hi, ${user.firstname && user.firstname.length > 0 ? user.firstname : user.username}</h1>
       <p>You'll need to verify your account before you can use the API. Just click on this link and you'll be all set:</p>
       <a target="_blank" href="https://inthehouse.dev/verify?id=${user.id}&hash=${user.verification_hash}">Verify your account.</a>
     `,
