866cdf954855e25136f565a0d4206c35cdd23950
stephenpg <stephenpalgeorge@gmail.com>
2021-06-03 17:12:01 +0100

    Refactor the `mail` helper
    Remove all 'nodemailer' code and use `sendgrid` stuff instead. This also simplified things a little as we don't need all the different transports anymore.

--- apps/api/src/app/helpers/mail.helper.ts
+++ apps/api/src/app/helpers/mail.helper.ts
@@ -1,53 +1,20 @@
-import * as nodemailer from 'nodemailer';
+import * as sendgrid from '@sendgrid/mail';
 import { EmailTemplates, IEmailTemplate, IUser } from "@in-the-house/api-interfaces";
 
 import templates from '../_data/email-templates';
 
-const admin_transporter = nodemailer.createTransport({
-  host: "mail.privateemail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.ADMIN_EMAIL_USER,
-    pass: process.env.ADMIN_EMAIL_PASSWORD,
-  },
-});
-
-const hello_transporter = nodemailer.createTransport({
-  host: "mail.privateemail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.HELLO_EMAIL_USER,
-    pass: process.env.HELLO_EMAIL_PASSWORD,
-  },
-});
-
-const support_transporter = nodemailer.createTransport({
-  host: "mail.privateemail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.SUPPORT_EMAIL_USER,
-    pass: process.env.SUPPORT_EMAIL_PASSWORD,
-  },
-});
-
-const transporters = {
-  admin: admin_transporter,
-  hello: hello_transporter,
-  support: support_transporter,
-};
-
-export function send(template: EmailTemplates, user: IUser, data = {}) {
-  const email: IEmailTemplate = templates[template]({ user, data });
-  const transporter = transporters[email.account];
-  const mailOptions = {
-    from: email.sender,
-    to: user.email,
-    subject: email.subject,
-    html: email.contents,
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+export async function send(template: EmailTemplates, user: IUser, data = {}) {
+  try {
+    const email: IEmailTemplate = templates[template]({ user, data });
+    const msg = {
+      to: user.email,
+      from: email.sender,
+      subject: email.subject,
+      html: email.contents,
+    };
+    await sendgrid.send(msg);
+  } catch (err) {
+    console.error(err);
   }
-
-  transporter.sendMail(mailOptions);
-}
+};
